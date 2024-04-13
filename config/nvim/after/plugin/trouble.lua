73227168d69475bdb2d6a62a23a37880ea1686c0
Nathan Gomez <gomez.fede98@gmail.com>
2024-04-13 12:02:10 -0400

    keybindings for trouble

--- config/nvim/after/plugin/trouble.lua
+++ config/nvim/after/plugin/trouble.lua
@@ -30,8 +30,8 @@ trouble.setup({
 		hover = "K", -- opens a small popup with the full multiline message
 		preview = "p", -- preview the diagnostic location
 		open_code_href = "c", -- if present, open a URI with more information about the diagnostic error
-		close_folds = { "zM", "zm" }, -- close all folds
-		open_folds = { "zR", "zr" }, -- open all folds
+		close_folds = { "zF", "zf" }, -- close all folds
+		open_folds = { "zO", "zo" }, -- open all folds
 		toggle_fold = { "zA", "za" }, -- toggle fold of current file
 		previous = "k", -- previous item
 		next = "j", -- next item
