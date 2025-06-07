a6b06997baa0cbed260a09d5871e894fa17926b5
Nathan Gomez <gomez.fede98@gmail.com>
2025-06-07 19:15:46 -0300

    nvim: easymotions autocmds

--- config/nvim/lua/plugins/easymotions.lua
+++ config/nvim/lua/plugins/easymotions.lua
@@ -1,4 +1,22 @@
 return {
   "easymotion/vim-easymotion",
-  lazy = false,
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local autocmd = vim.api.nvim_create_autocmd
+
+    autocmd("User", {
+      pattern = "EasyMotionPromptBegin",
+      callback = function()
+        vim.diagnostic.disable(0)
+      end,
+    })
+
+    autocmd("User", {
+      pattern = "EasyMotionPromptEnd",
+      callback = function()
+        vim.diagnostic.enable()
+      end,
+    })
+    return {}
+  end,
 }
