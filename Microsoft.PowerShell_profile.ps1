a813e26bb85305b8ddbf648d4bebd1c56913cc31
Nathan Gomez <gomez.fede98@gmail.com>
2025-12-15 18:06:12 -0300

    pwsh update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
+# Fzf Configurtation
+# Install-Module -Name PSFzf -Scope CurrentUser
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' -PSReadlineChordProvider 'Ctrl+d'
+
 Set-Alias -Name lzg -Value lazygit
 function notes { Set-Location "F:\Fede\gdrive\notes" }
+function startup { Set-Location "C:\Users\gomez\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" }
 function reload { . $PROFILE }
