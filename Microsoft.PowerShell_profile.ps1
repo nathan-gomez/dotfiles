e4c838fc656f5978820d7d1489a80599e19cf190
Nathan Gomez <gomez.fede98@gmail.com>
2026-04-02 15:37:08 -0300

    pwsh

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,30 @@
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' -PSReadlineChordProvider 'Ctrl+d'
 
+$env:EDITOR = "nvim"
+
 Set-Alias -Name lzg -Value lazygit
 Set-Alias -Name vi -Value nvim
 function notes { Set-Location "F:\Fede\gdrive\notes" }
 function startup { Set-Location "C:\Users\gomez\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" }
 function reload { . $PROFILE }
+
+function api-person {
+  cd W:/Backend/PValue.API.Person
+  dotnet watch run --urls="http://localhost:50571"
+}
+
+function api-author {
+  cd W:/Backend/PValue.API.Author
+  dotnet watch run --urls="http://localhost:50575"
+}
+
+function api-market {
+  cd W:/Backend/PValue.API.Market
+  dotnet watch run --urls="http://localhost:63675"
+}
+
+function api-admin {
+  cd W:/Backend/PValue.API.Admin
+  dotnet watch run --urls="http://localhost:50579"
+}
